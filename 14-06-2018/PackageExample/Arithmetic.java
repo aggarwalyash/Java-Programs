2a93a734eceeccda258bfe1ad9fc7fff03be0440
Yash Aggarwal <yashaggarwal212@gmail.com>
2018-06-14 16:09:53 +0530

    Add files via upload

---
+++ 14-06-2018/PackageExample/Arithmetic.java
@@ -0,0 +1,9 @@
+import calc.Calculator;
+class Arithmetic{
+	public static void main(String args[]){
+		Calculator calc = new Calculator();
+		System.out.println(calc.add(5,4));
+		System.out.println(calc.sub(5,4));
+	}
+
+}
